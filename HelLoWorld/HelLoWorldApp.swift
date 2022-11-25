b9e6885ac9e519097d636717688f88c9d7d0bdc4
Kovalenko <kovalenko1988@icloud.com>
2022-11-25 12:40:02 +0700

    Initial Commit

---
+++ HelLoWorld/HelLoWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelLoWorldApp.swift
+//  HelLoWorld
+//
+//  Created by Николай Коваленко on 25.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct HelLoWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
